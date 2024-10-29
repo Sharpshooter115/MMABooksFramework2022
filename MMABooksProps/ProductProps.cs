75c818bcd018134823d52ad424522bf0c1c40a37
Brody Herrera <brody.herrera@oit.edu>
2024-10-28 23:13:30 -0700

    finshed product props need to work on db next

--- MMABooksProps/ProductProps.cs
+++ MMABooksProps/ProductProps.cs
@@ -1,12 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MMABooksTools;
+using DBDataReader = MySql.Data.MySqlClient.MySqlDataReader;
+using System.Text.Json;
 
 namespace MMABooksProps
 {
-    internal class ProductProps
+    [Serializable()]
+    public class ProductProps : IBaseProps
     {
+        #region Auto-implemented Properties
+
+        public int ProductID { get; set; } = 0;
+        public string ProductCode { get; set; } = "";
+        public string Description { get; set; } = "";
+        public decimal UnitPrice { get; set; } = 0.0m;
+        public int OnHandQuantity { get; set; } = 0;
+
+        public int ConcurrencyID { get; set; } = 0;
+        #endregion
+
+        public object Clone()
+        {
+            return new ProductProps();
+            {
+                ProductID = this.ProductID ,
+                ProductCode = this.ProductCode ,
+                Description = this.Description ,
+                UnitPrice = this.UnitPrice ,
+                OnHandQuantity = this.OnHandQuantity ,
+                ConcurrencyID = this.ConcurrencyID
+
+
+            };
+
+        }
+        public string GetState()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+        public void SetState(string jsonString)
+        {
+            var props = JsonSerializer.Deserialize<ProductProps>(jsonString);
+            if (props != null)
+            {
+                this.ProductID = props.ProductID;
+                this.ProductCode = props.ProductCode;
+                this.Description = props.Description;
+                this.UnitPrice = props.UnitPrice;
+                this.OnHandQuantity = props.OnHandQuantity;
+                this.ConcurrencyID = props.ConcurrencyID;
+            }
+        }
+        public void SetState(DBDataReader dr)
+        {
+            this.ProductID = (int)dr["ProductID"];
+            this.ProductCode = dr["ProductCode"].ToString().Trim();
+            this.Description = dr["Description"].ToString().Trim();
+            this.UnitPrice = (decimal)dr["UnitPrice"];
+            this.OnHandQuantity = (int)dr["OnHandQuantity"];
+            this.ConcurrencyID = (int)dr["ConcurrencyID"];
+        }
     }
 }
